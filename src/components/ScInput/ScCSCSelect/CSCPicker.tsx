590711c379d80760a2e247f575447fb6758a3209
hnmht <cnmht369@gmail.com>
2026-01-27 10:29:52 +0800

    Complete SCCSCSelect component

---
+++ src/components/ScInput/ScCSCSelect/CSCPicker.tsx
@@ -0,0 +1,156 @@
+import { useState, useEffect } from "react";
+import { View, ScrollView, TouchableOpacity } from "react-native";
+import { Text, useTheme, SegmentedButtons, Card } from "react-native-paper";
+import { TFunction } from "i18next";
+
+import ScPubTree from "../ScPubTree/ScPubTree";
+import DocList from "../../DocList/DocList";
+import { SimpCSC } from "../../../dataType/types/csc";
+import ScHandSwitch from "../../ScHandSwitch/ScHandSwitch";
+import { simpCSCRepo } from "../../../db/crud/csc";
+
+interface CSCPickerProps {
+    pressItemAction: (item: SimpCSC) => void;
+    cancelAction: () => void;
+    currentItem: SimpCSC;
+    t: TFunction
+}
+
+// Convert an Array of SimpCSC object into an array of SimpCSC IDs
+const convertCSCsToIDs = (csc: SimpCSC) => {
+    let selectDocIds: number[] = [];
+    selectDocIds.push(csc.id);
+    return selectDocIds;
+};
+
+const SICPicker = ({ pressItemAction, cancelAction, currentItem, t }: CSCPickerProps) => {
+    const [docs, setDocs] = useState<SimpCSC[]>([]);
+    const [allOrRecent, setAllOrRecent] = useState<"recent" | "all">("recent");
+    const theme = useTheme();
+    const selectedDocIds = convertCSCsToIDs(currentItem);
+
+    const handleInitDocs = (allFlag = allOrRecent) => {
+        let localDocs: SimpCSC[] = [];
+        if (allFlag === "all") {
+            localDocs = simpCSCRepo.getAllData()
+        } else {
+            localDocs = simpCSCRepo.getRecent();
+        }
+        setDocs(localDocs);
+    };
+
+    useEffect(() => {
+        handleInitDocs();
+    }, []);
+
+    // Switch SegmentedButtons
+    const handleChangeSeg = (value: "recent" | "all") => {
+        setAllOrRecent(value);
+        handleInitDocs(value);
+    };
+
+    // Actions after press SimpCSC item
+    const handlePress = (item: SimpCSC) => {
+        if (allOrRecent === "all") {
+            simpCSCRepo.addRecent(item);
+        }
+        pressItemAction(item);
+    };
+    // Actions after long press SimpCSC item
+    const handleLongPress = (item: SimpCSC) => {
+        if (allOrRecent === "recent") {
+            simpCSCRepo.deleteRecent(item);
+            handleInitDocs();
+        }
+        return
+    };
+
+    // Refresh
+    const handleDocRefresh = async () => {
+        await simpCSCRepo.initCache();
+        handleInitDocs(allOrRecent);
+    };
+
+    const SICCard = ({ item }: { item: SimpCSC }) => {
+        return (
+            <Card key={item.id} style={{ marginTop: 2, marginBottom: 2 }}>
+                <TouchableOpacity
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        flexWrap: "wrap",
+                        margin: 4
+                    }}
+                    onPress={() => pressItemAction(item)}
+                    onLongPress={() => handleLongPress(item)}
+                >
+                    <Text style={{ width: "100%", padding: 2, fontWeight: "bold", color: item.status === 1 ? "red" : theme.colors.onBackground }}>{t("name")}: {item.name}</Text>
+                    <Text style={{ width: "100%", padding: 2 }}>{t("description")} : {item.description}</Text>
+                    <Text style={{ width: "100%", padding: 2 }}>{t("status")}: {t(item.status === 0 ? "normal" : "disable")}</Text>
+                </TouchableOpacity>
+            </Card>
+        );
+    };
+
+    return (
+        <View style={{ flex: 1 }}>
+            <View style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                minHeight: 40,
+                width: "100%",
+                backgroundColor: theme.colors.background
+            }}>
+                <View style={{ padding: 4, minHeight: 40, width: "100%", display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                    <Text variant="titleMedium">{t("chooseCategory")}</Text>
+                </View>
+                <View style={{ width: "100%", minHeight: 42, padding: 2 }}>
+                    <SegmentedButtons
+                        value={allOrRecent}
+                        onValueChange={(value: "recent" | "all") => handleChangeSeg(value)}
+                        buttons={[
+                            {
+                                value: "recent",
+                                label: t("recents")
+                            },
+                            {
+                                value: "all",
+                                label: t("all")
+                            }
+                        ]}
+                    />
+                </View>
+            </View>
+            {allOrRecent === "all"
+                ? <ScrollView>
+                    <ScPubTree
+                        docName={t("category")}
+                        isDisplayAll={false}
+                        oriDocs={docs}
+                        onDocPress={handlePress}
+                        selectDocIDs={selectedDocIds}
+                        onDocLongPress={handleLongPress}
+                        isEdit={true}
+                    />
+                </ScrollView>
+                : <DocList
+                    rows={docs}
+                    ItemElement={SICCard}
+                    rowsPerPage={10}
+                    searchFields={["name", "description"]}
+                    sortFunction={(a, b) => a.id - b.id}
+                    refreshing={false}
+                />
+            }
+            <ScHandSwitch
+                docRefresh={handleDocRefresh}
+                cancelAction={cancelAction}
+            />
+
+        </View>
+    )
+};
+
+export default SICPicker;
---
+++ src/components/ScInput/ScCSCSelect/ScCSCSelect.tsx
@@ -0,0 +1,141 @@
+import { memo, useState, useEffect } from "react";
+import { View, Modal, Alert } from "react-native";
+import { TextInput, useTheme } from "react-native-paper";
+
+import CSCDetail from "./CSCDetail";
+import CSCPicker from "./CSCPicker";
+import { ScInputProps } from "../../../dataType/types/scInput";
+import { ScDataTypeList } from "../../../dataType/types/scDataType";
+import { useAppSelector } from "../../../store/hooks";
+import { getEmptyCSC } from "../../../dataType/dataZero/csc";
+import { useTranslation } from "react-i18next";
+import { SimpCSC } from "../../../dataType/types/csc";
+
+const zeroValue = getEmptyCSC();
+// 525 Seacloud Construction Site Category Select Input
+const SCCSCSelect = (props: ScInputProps<ScDataTypeList.SimpCSC>) => {
+    const {
+        positionID = 0,
+        rowIndex = 0,
+        allowNull = true,
+        isEdit = false,
+        itemShowName = "",
+        itemKey,
+        initValue = zeroValue,
+        pickDone,
+        placeholder = "",
+        errInfo = { isErr: false, msg: "" },
+        width = "100%",
+        height = 68
+    } = props;
+    const [currentDoc, setCurrentDoc] = useState(initValue);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [detailOpen, setDetailOpen] = useState(false);
+    const theme = useTheme();
+    const { t } = useTranslation();
+    const label = allowNull ? t(itemShowName) : "*" + t(itemShowName);
+    // Switch button commands
+    const { buttonPosition } = useAppSelector(state => state.swapPosition);
+    useEffect(() => {
+        setCurrentDoc(initValue);
+    }, [initValue]);
+
+    // Check Value and pass data to the parents component
+    const handleTransfer = async (doc = currentDoc) => {
+        if (!isEdit) {
+            return
+        }
+        pickDone(doc, itemKey, positionID, rowIndex, errInfo);
+    };
+
+    // Actions after press cancel button in CSCPicker modal
+    const handleCancelAction = () => {
+        setDialogOpen(false);
+    };
+    // Actions after press item in CscPicker modal
+    const handlePressItemAction = (item: SimpCSC) => {
+        setCurrentDoc(item);
+        handleTransfer(item);
+        setDialogOpen(false);
+    };
+    // Actions after press clear button
+    const handleClear = () => {
+        if (!allowNull) {
+            return
+        }
+        setCurrentDoc(zeroValue);
+        handleTransfer(zeroValue);
+    };
+
+    return (
+        <View id={`view${itemKey}${positionID}${rowIndex}`} style={{ width: width, height: height, padding: 2 }}>
+            <TextInput
+                id={`textinput${itemKey}${positionID}${rowIndex}`}
+                mode="outlined"
+                keyboardType="default"
+                label={label}
+                placeholder={isEdit ? placeholder : ""}
+                editable={false}
+                disabled={!isEdit}
+                value={currentDoc.name}
+                error={errInfo.isErr}
+                left={buttonPosition === "right"
+                    ? errInfo.isErr
+                        ? <TextInput.Icon
+                            icon="alert"
+                            color={theme.colors.error}
+                            onPress={() => Alert.alert(t("err"), errInfo.msg)}
+                        />
+                        : null
+                    : <TextInput.Icon
+                        icon="home-city"
+                        color={isEdit ? theme.colors.primary : theme.colors.secondary}
+                        onPress={isEdit ? () => setDialogOpen(true) : () => setDetailOpen(true)}
+                        onLongPress={isEdit ? () => handleClear() : undefined}
+                    />
+                }
+                right={buttonPosition === "left"
+                    ? errInfo.isErr
+                        ? <TextInput.Icon
+                            icon="alert"
+                            color={theme.colors.error}
+                            onPress={() => Alert.alert(t("err"), errInfo.msg)}
+                        />
+                        : null
+                    : <TextInput.Icon
+                        icon="home-city"
+                        color={isEdit ? theme.colors.primary : theme.colors.secondary}
+                        onPress={isEdit ? () => setDialogOpen(true) : () => setDetailOpen(true)}
+                        onLongPress={isEdit ? () => handleClear() : undefined}
+                    />
+
+                }
+                style={{ width: "100%" }}
+            />
+            <Modal
+                visible={dialogOpen}
+                id={`modal${itemKey}${positionID}${rowIndex}`}
+            >
+                <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+                    <CSCPicker
+                        cancelAction={handleCancelAction}
+                        pressItemAction={handlePressItemAction}
+                        currentItem={currentDoc}
+                        t={t}
+                    />
+                </View>
+            </Modal>
+            {isEdit
+                ? null
+                : <CSCDetail
+                    currentItem={currentDoc}
+                    visible={detailOpen}
+                    backAction={() => setDetailOpen(false)}
+                    t={t}
+                />
+            }
+        </View>
+    );
+};
+export default memo(SCCSCSelect);
+
