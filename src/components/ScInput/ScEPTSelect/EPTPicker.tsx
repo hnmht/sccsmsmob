fff6d5f1e6351509df94ede7768bebe88b39037d
hnmht <cnmht369@gmail.com>
2026-02-04 10:09:19 +0800

    Complete ScEPTSelect Component

---
+++ src/components/ScInput/ScEPTSelect/EPTPicker.tsx
@@ -0,0 +1,111 @@
+import { useState, useEffect } from "react";
+import { View, TouchableOpacity } from "react-native";
+import { Card, Text, useTheme } from "react-native-paper";
+import { TFunction } from "i18next";
+import DocList from "../../DocList/DocList";
+import { EPT } from "../../../dataType/types/ept";
+import { EPTRepo } from "../../../db/crud/ept";
+import ScSegmentAllOrRecent from "../../ScSegmentAllOrRecent/ScSegmentAllOrRecent";
+import ScHandSwitch from "../../ScHandSwitch/ScHandSwitch";
+
+interface EPTPickerProps {
+    pressItemAction: (item: EPT) => void;
+    cancelAction: () => void;
+    currentItem: EPT;
+    t: TFunction
+}
+
+const EPTPicker = ({ cancelAction, pressItemAction, currentItem, t }: EPTPickerProps) => {
+    const theme = useTheme();
+    const [docs, setDocs] = useState<EPT[]>([]);
+    const [allOrRecent, setAllOrRecent] = useState<"recent" | "all">("recent");
+
+    const handleInitDocs = (allFlag = allOrRecent) => {
+        let localDocs = [];
+        if (allFlag === "all") {
+            localDocs = EPTRepo.getAllData();
+        } else {
+            localDocs = EPTRepo.getRecent();
+        }
+        setDocs(localDocs);
+    };
+
+    useEffect(() => {
+        handleInitDocs();
+    }, []);
+
+    // Switch SegmentedButtons
+    const handleChangeSeg = (value: "recent" | "all") => {
+        setAllOrRecent(value);
+        handleInitDocs(value);
+    };
+
+    // Actions after press Execution Project Template item
+    const handlePress = (item: EPT) => {
+        if (allOrRecent === "all") {
+            EPTRepo.addRecent(item);
+        }
+        pressItemAction(item);
+    };
+    // Refresh
+    const handleRefresh = async () => {
+        await EPTRepo.initCache();
+        handleInitDocs(allOrRecent);
+    };
+
+    // Actions after long press Execution Project Template item
+    const handleLongPress = (item: EPT) => {
+        if (allOrRecent === "recent") {
+            EPTRepo.deleteRecent(item);
+            handleInitDocs();
+        }
+        return
+    };
+
+    const EPTCard = ({ item }: { item: EPT }) => {
+        return (
+            <Card key={item.id} style={{ marginTop: 2, marginBottom: 4 }}>
+                <TouchableOpacity
+                    style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        flexWrap: "wrap",
+                        margin: 4
+                    }}
+                    onPress={() => handlePress(item)}
+                    onLongPress={() => handleLongPress(item)}
+                >
+                    <Text style={{ width: "100%", padding: 2, fontWeight: "bold", color: item.status === 1 ? "red" : theme.colors.onBackground }}>{t("name")} : {item.name}</Text>
+                    <Text style={{ width: "100%", padding: 2 }}>{t("code")} : {item.code}</Text>
+                    <Text style={{ width: "100%", padding: 2 }}>{t("status")} : {t(item.status === 0 ? "normal" : "disable")}</Text>
+                    <Text style={{ width: "100%", padding: 2 }}>{t("description")} : {item.description}</Text>
+                </TouchableOpacity>
+            </Card>
+        );
+    };
+
+    return (
+        <View style={{ flex: 1 }}>
+            <ScSegmentAllOrRecent
+                title={t("chooseTemplate")}
+                allOrRecent={allOrRecent}
+                setAllOrRecent={handleChangeSeg}
+            />
+            <DocList
+                rows={docs}
+                ItemElement={EPTCard}
+                rowsPerPage={10}
+                searchFields={["code", "name", "description"]}
+                sortFunction={(a, b) => a.id - b.id}
+                refreshing={false}
+            />
+            <ScHandSwitch
+                docRefresh={handleRefresh}
+                cancelAction={cancelAction}
+            />
+
+        </View>
+    );
+};
+
+export default EPTPicker;
---
+++ src/components/ScInput/ScEPTSelect/ScEPTSelect.tsx
@@ -0,0 +1,141 @@
+import { memo, useState, useEffect } from "react";
+import { View, Modal, Alert } from "react-native";
+import { TextInput, useTheme } from "react-native-paper";
+import { useTranslation } from "react-i18next";
+import EPTDetail from "./EPTDetail";
+import EPTPicker from "./EPTPicker";
+import { getEmptyEPT } from "../../../dataType/dataZero/ept";
+import { ScInputProps } from "../../../dataType/types/scInput";
+import { ScDataTypeList } from "../../../dataType/types/scDataType";
+import { useAppSelector } from "../../../store/hooks";
+import { EPT } from "../../../dataType/types/ept";
+
+const zeroValue = getEmptyEPT();
+
+//580 Seacloud Execution Project Template select input component
+const ScEPTSelect = (props: ScInputProps<ScDataTypeList.EPT>) => {
+    const {
+        positionID = 0,
+        rowIndex = 0,
+        allowNull = true,
+        isEdit = false,
+        itemShowName = "",
+        itemKey,
+        initValue = zeroValue,
+        pickDone,
+        placeholder = "",
+        errInfo = { isErr: false, msg: "" },
+        width = "100%",
+        height = 68,
+    } = props;
+    const [currentDoc, setCurrentDoc] = useState<EPT>(initValue);
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [detailOpen, setDetailOpen] = useState(false);
+    const theme = useTheme();
+    const { t } = useTranslation();
+    const label = allowNull ? t(itemShowName) : "*" + t(itemShowName);
+    // Switch button position
+    const { buttonPosition } = useAppSelector(state => state.swapPosition);
+
+    useEffect(() => {
+        setCurrentDoc(initValue);
+    }, [initValue]);
+    // Check Value and pass data to the parents component
+    const handleTransfer = async (doc = currentDoc) => {
+        if (!isEdit) {
+            return
+        }
+        pickDone(doc, itemKey, positionID, rowIndex, errInfo);
+    };
+
+    // Actions after press cancel button in the EPTPicker modal
+    const handleCancelAction = () => {
+        setDialogOpen(false);
+    };
+    // Actions after press EPT item in the EPTPicker modal
+    const handlePressItemAction = (item: EPT) => {
+        setCurrentDoc(item);
+        handleTransfer(item);
+        setDialogOpen(false);
+    };
+    // Actions after press clear button 
+    const handleClear = () => {
+        if (!allowNull) {
+            return
+        }
+        setCurrentDoc(zeroValue);
+        handleTransfer(zeroValue);
+    };
+
+    return (
+        <View id={`view${itemKey}${positionID}${rowIndex}`} style={{ width: width, height: height, padding: 2 }}>
+            <TextInput
+                id={`textinput${itemKey}${positionID}${rowIndex}`}
+                mode="outlined"
+                keyboardType="default"
+                label={label}
+                placeholder={isEdit ? placeholder : ""}
+                editable={false}
+                disabled={!isEdit}
+                value={currentDoc.name}
+                error={errInfo.isErr}
+                left={buttonPosition === "right"
+                    ? errInfo.isErr
+                        ? <TextInput.Icon
+                            icon="alert"
+                            color={theme.colors.error}
+                            onPress={() => Alert.alert(t("err"), errInfo.msg)}
+                        />
+                        : null
+                    : <TextInput.Icon
+                        icon="format-list-numbered"
+                        color={isEdit ? theme.colors.primary : theme.colors.secondary}
+                        onPress={isEdit ? () => setDialogOpen(true) : () => setDetailOpen(true)}
+                        onLongPress={handleClear}
+                    />
+                }
+                right={buttonPosition === "left"
+                    ? errInfo.isErr
+                        ? <TextInput.Icon
+                            icon="alert"
+                            color={theme.colors.error}
+                            onPress={() => Alert.alert(t("err"), errInfo.msg)}
+                        />
+                        : null
+                    : <TextInput.Icon
+                        icon="format-list-numbered"
+                        color={isEdit ? theme.colors.primary : theme.colors.secondary}
+                        onPress={isEdit ? () => setDialogOpen(true) : () => setDetailOpen(true)}
+                        onLongPress={handleClear}
+                    />
+                }
+                style={{ width: "100%" }}
+            />
+            <Modal
+                visible={dialogOpen}
+                id={`modal${itemKey}${positionID}${rowIndex}`}
+            >
+                <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+                    <EPTPicker
+                        cancelAction={handleCancelAction}
+                        pressItemAction={handlePressItemAction}
+                        currentItem={currentDoc}
+                        t={t}
+                    />
+                </View>
+            </Modal>
+            {isEdit
+                ? null
+                : <EPTDetail
+                    currentItem={currentDoc}
+                    visible={detailOpen}
+                    backAction={() => setDetailOpen(false)}
+                    t={t}
+                />
+            }
+        </View>
+    );
+};
+
+export default memo(ScEPTSelect);
+
